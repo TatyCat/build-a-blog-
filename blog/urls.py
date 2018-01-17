6f6a216e176e2d03168ba647c9ab65a7a1368d7f
TatyCat <williams.tatyanna@gmail.com>
2018-01-16 22:55:40 -0500

    Added functionality for main blog page, detail page and add new post page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list_base, name='post_list_base'),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^blog/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^blog/new/$', views.post_new, name='post_new'),
+    url(r'^blog/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    
 ]
